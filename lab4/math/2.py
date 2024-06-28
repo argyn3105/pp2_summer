9d7060badd20b70b78265f992618e78810da0d40
Argyn Amantay <114258809+argyn3105@users.noreply.github.com>
2024-06-28 10:31:30 +0500

    Add files via upload

---
+++ lab4/math/2.py
@@ -0,0 +1,9 @@
+def trap(a, b, h):
+    A = ((a + b) * h) / 2
+    return A
+x = int(input("Input 1st base: "))
+y = int(input("Input 2nd base: "))
+z = int(input("Input height: "))
+print(trap(x, y, z))
+
+#trapezoid area
---
+++ lab4/math/4.py
@@ -0,0 +1,5 @@
+a = int(input("Input the length: "))
+b = int(input("Input the height: "))
+print(a * b)
+
+#parallelogram area
