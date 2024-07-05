daf53954c03523947138c8757d21f216b96fabbc
Argyn Amantay <114258809+argyn3105@users.noreply.github.com>
2024-07-05 10:03:18 +0500

    Add files via upload

---
+++ lab5/3.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = r'[a-z]+_[a-z]+'
+
+string = 'hello_world is a popular programming convention'
+
+match = re.findall(pattern, string)
+
+print(match)
+#hello_world
---
+++ lab5/5.py
@@ -0,0 +1,13 @@
+import re
+
+pattern = r'a.+b$' #. - any character
+
+string = "The quick brown fox jumps over a lazy dog b"
+
+match = re.search(pattern, string)
+
+if match:
+    print("Match found:", match.group())
+else:
+    print("No match found.")
+    #a_b
